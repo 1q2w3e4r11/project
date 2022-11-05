90cde9219553eafe7a32607ed44ea04a85240ecd
aa <kmc_n@naver.com>
2022-11-05 21:02:07 +0900

    Improved Divide function

--- Divide.py
+++ Divide.py
@@ -1,2 +1,10 @@
 def divide (x,y):
-    return x/y
+    try:
+        a = x/y
+    except:
+        print("ZeroDivisionError");
+        f = open("stdout2.txt", 'a')
+        print("0 input : ",x, y,  file=f)
+        f.close() 
+    else:
+        return a
